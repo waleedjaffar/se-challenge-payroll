a9253120f853931e4cd99d760e204206caca782e
Waleed Jaffar <waleed.jaffar@goomo.com>
2018-08-06 09:22:24 +0530

    added more sspecs to job groups

--- spec/models/job_group_spec.rb
+++ spec/models/job_group_spec.rb
@@ -7,4 +7,20 @@ RSpec.describe JobGroup, type: :model do
   it 'returns atleast two groups' do
     expect(job_groups.count).to be >= 2
   end
+
+  it 'name is mandatory' do 
+    job_group = JobGroup.new(name: nil, hour_rate: 40)
+    job_group.valid?
+    expect(job_group.errors[:name]).to eq (["can't be blank"])
+  end
+
+  it 'does not allow creation of duplicate group' do
+    job_group = JobGroup.create(name: job_groups.first.name, hour_rate: job_groups.first.hour_rate)
+    expect(job_group.errors[:name]).to eq(['has already been taken'])
+  end
+
+  it 'hour rate defaults to 0 if not available' do
+    job_group = JobGroup.create(name: 'Test')
+    expect(job_group.hour_rate).to eq 0
+  end
 end
